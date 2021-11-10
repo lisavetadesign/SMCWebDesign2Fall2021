a8f0bb21b9c245cc49165dacd5ebd13086b29f4d
lisavetadesign <79555711+lisavetadesign@users.noreply.github.com>
2021-11-09 21:23:46 -0800

    Week 10 HW

--- Week10/HW/script.js
+++ Week10/HW/script.js
@@ -1,16 +1,34 @@
-let menuIconToggle = document.getElementById("iconImg");
+const menu = document.querySelector('#mobileMenu');
+const menuLinks = document.querySelector('.nav-menu');
 
-console.log(toggleMenuIcon);
+let imgShowcase = document.getElementById("imageShowcase");
+let imgOneBtn = document.getElementById("imgOne");
+let imgTwoBtn = document.getElementById("imgTwo");
+let imgThreeBtn = document.getElementById("imgThree");
+let imgFourBtn = document.getElementById("imgFour");
+let imgFiveBtn = document.getElementById("imgFive");
 
-let toggleMenuIcon = function(){
-    if (menuIconToggle.alt == "hamberger menu"){
-        menuIconToggle.src = "menuIcon02.png";
-        menuIconToggle.alt = "menu exit";
-    }
-    else{
-        menuIconToggle.src = "menuIcon01.png";
-        menuIconToggle.alt = "hamberger menu";
-    }
-}
+menu.addEventListener("click", function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
 
-menuIconToggle.addEventListener("click", toggleMenuIcon);
+imgOneBtn.addEventListener("click", function(){ 
+    imgShowcase.style.backgroundImage = "url(" + "GreatSmokyMountainsNP.jpg" + ")" ;
+})
+
+imgTwoBtn.addEventListener("click", function(){ 
+    imgShowcase.style.backgroundImage = "url(" + "YellowstoneNP.jpg" + ")" ;
+})
+
+imgThreeBtn.addEventListener("click", function(){ 
+    imgShowcase.style.backgroundImage = "url(" + "ZionNP.jpeg" + ")" ;
+})
+
+imgFourBtn.addEventListener("click", function(){ 
+    imgShowcase.style.backgroundImage = "url(" + "RockyMountainNP.jpg" + ")" ;
+})
+
+imgFiveBtn.addEventListener("click", function(){ 
+    imgShowcase.style.backgroundImage = "url(" + "GrandTetonNP.jpg" + ")" ;
+})
