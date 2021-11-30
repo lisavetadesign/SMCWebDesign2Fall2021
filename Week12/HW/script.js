e0a6357f77b985eeccb42d5ca0b39d338cb9077b
lisavetadesign <79555711+lisavetadesign@users.noreply.github.com>
2021-11-30 14:26:31 -0800

    Week 12 HW
    Some elements are videos so they will not be displayed

---
+++ Week12/HW/script.js
@@ -0,0 +1,26 @@
+let NASAImgBttn = document.getElementById('getNASAImage');
+let NASAImgDisp = document.getElementById('displayNASAImage');
+
+let getNASAImage = function(){
+    let baseURL = 'https://api.nasa.gov/planetary/apod?date=';
+    let dateURL = NASAImgBttn.value;
+    let apiURL = 'R4dsEdQ4oiu3yNuRVufMSnJOgQzUoY4m3GDr8eyj';
+    let imgURL = baseURL + dateURL + '&api_key=' + apiURL;
+
+    fetch(imgURL).
+        then(function(result){
+            return result.json();
+        }).
+        then(function(data){
+            while(NASAImgDisp.firstChild){
+                NASAImgDisp.removeChild(NASAImgDisp.firstChild);
+            }
+        
+        let img = document.createElement('img');
+        img.src = data.url;
+        img.alt = 'NASA daily image is a Video :/';
+        NASAImgDisp.appendChild(img);
+        })
+}
+
+NASAImgBttn.addEventListener('change', getNASAImage);
