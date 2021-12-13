06d97caaa849948d35412cc699a9b11a394a7909
lisavetadesign <79555711+lisavetadesign@users.noreply.github.com>
2021-12-13 14:14:20 -0800

    Week 14 Final
    Final project progression

---
+++ Week14/HW/script/menuscript.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#mobileMenu');
+let menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener("click", function(){
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+})
