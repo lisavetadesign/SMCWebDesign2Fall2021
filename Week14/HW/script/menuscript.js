49f6493d840a43d1106ebd2952da19d0e6e29d6f
lisavetadesign <79555711+lisavetadesign@users.noreply.github.com>
2021-12-14 15:59:54 -0800

    Final
    Week 14

--- Week14/HW/script/menuscript.js
+++ Week14/HW/script/menuscript.js
@@ -4,4 +4,5 @@ let menuLinks = document.querySelector('.nav-menu');
 menu.addEventListener("click", function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
-})
+});
+console.log("here")
