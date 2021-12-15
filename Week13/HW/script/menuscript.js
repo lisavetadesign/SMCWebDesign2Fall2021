f2647cb1f376153f53836f40af240329b904145f
lisavetadesign <79555711+lisavetadesign@users.noreply.github.com>
2021-12-14 17:43:52 -0800

    commits

--- Week13/HW/script/menuscript.js
+++ Week13/HW/script/menuscript.js
@@ -1,21 +1,7 @@
-let menubttn = document.getElementById("menubutton")
-let nav = document.getElementById("navigation");
+let menu = document.querySelector('#mobileMenu');
+let menuLinks = document.querySelector('.nav-menu');
 
-let toggleImage = function(){
-  if (nav.style.display === "none") {
-    nav.style.display = "block";
-  } else {
-    nav.style.display = "none";
-  }
-    if (menubttn.alt == "menu_open") {
-        menubttn.src = "menubars.png";
-        menubttn.alt = "menu_close";
-    }
-    else {
-        menubttn.src = "menux.png";
-        menubttn.alt = "menu_open";
-     }
-    
-}
-
-menubttn.addEventListener('click', toggleImage);
+menu.addEventListener("click", function(){
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+})
